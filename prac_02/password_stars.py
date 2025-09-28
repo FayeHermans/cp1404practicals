fac2dc763152c70c5c7fc186d8f0f6d734d6d212
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-09-28 14:28:50 +1000

    Add function dockstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,17 +1,20 @@
 """Password stars"""
 
 def main():
+    """Print a user password using stars"""
     minimum_length = 5
     password = get_password(minimum_length)
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print stars the same length as the password"""
     print("*" * len(password), end="")
     print()
 
 
 def get_password(minimum_length):
+    """Get user input longer than minimum length"""
     password = input("Enter password: ")
     while len(password) < minimum_length:
         print(f"Password has to be a minimum of {minimum_length} characters.")
