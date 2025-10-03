66cdf6c6aeece9846de22b869269aec9042e918b
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-10-03 10:39:57 +1000

    Complete part 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,8 @@ print(name, file=out_file) # I googled why this warning is here and can't figure
 out_file.close()
 
 # 2
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
+
