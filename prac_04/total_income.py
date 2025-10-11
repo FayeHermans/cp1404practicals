d0cd41172f757f7bfd8114b2dbcfd24d087e3296
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-10-11 15:41:43 +1000

    match printing with requirements

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -17,12 +17,13 @@ def main():
 
 
 def print_report(incomes, number_of_months):
+    """Print the months of incomes and the total looking like a grid."""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:10.2f}         Total: ${:10.2f}".format(month, income, total))
 
 
 main()
