7bf9e7e6191243c77810659701449a501fbaa228
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-10-31 18:56:42 +1000

    Add prac 06 programming language class file and program

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""
+Prac 06: Languages
+Est time: 15min
+Start time: 6:50pm
+Finnish time:
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
