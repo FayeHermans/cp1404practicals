21b5cb2315be5efafc8e05a009b0527b824ea910
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-09 16:30:20 +1000

    Trying to figure out how to fix date issue in filter menu option

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,8 +1,10 @@
+import datetime
+
 class Project:
 
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
@@ -10,3 +12,6 @@ class Project:
     def __str__(self):
        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percentage}%"
 
+
+    def __lt__(self, other):
+        return self.priority < other.priority
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,9 +2,12 @@
 Prac 07 - Project Management
 Est time: 55 min
 Start time: 2:17PM
+Break: 3:12PM - 3:44PM
 Finish time:
 Actual time:
 """
+from operator import itemgetter
+
 from project import Project
 import datetime
 
@@ -21,22 +24,49 @@ MENU = ("- (L)oad projects\n"
 def main():
     projects = []
     filename = 'projects.txt'
-    projects = process_file(filename, 'r', projects)
+    projects = load_file(filename, projects)
     print("Welcome to Pythonic Project Management")
     print(f"Loaded {len(projects)} projects from {filename}")
     choice = input(MENU).upper()
     while choice != "Q":
         if choice == "L":
-            # load project
-            pass
+            # load project from user input filename
+            user_filename = input("Enter filename to load files: ")
+            projects = load_file(user_filename, projects)
         elif choice == "S":
             # save project
-            pass
+            save_filename = input("Enter filename to save to: ")
+            save_file(save_filename, projects)
         elif choice == "D":
             # display projects
-            pass
+            incomplete = []
+            complete = []
+            for project in projects:
+                if project.completion_percentage == 100:
+                    complete.append(project)
+                else:
+                    incomplete.append(project)
+            incomplete.sort()
+            complete.sort()
+            print("Incomplete projects:")
+            for project in incomplete:
+                print(f"\t {project}")
+            print("Completed projects: ")
+            for project in complete:
+                print(f"\t {project}")
         elif choice == "F":
-            pass
+            date_string = input("Show projects that start after date (dd/mm/yy): ")
+            date_from = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+            to_do_projects = []
+            for project in sorted(projects, key=itemgetter(1)):
+                project_date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y")
+                if project_date >= date_from:
+                    to_do_projects.append(project)
+                    print(project.start_date)
+            #TODO fix typeError ????????
+
+            print(item for project in to_do_projects)
+
         elif choice == "A":
             pass
         elif choice == "U":
@@ -44,23 +74,29 @@ def main():
         else:
             print("Invalid choice.")
         choice = input(MENU).upper()
+    print("Thank you for using custom-built project management software.")
 
 
 
-
-def process_file(filename, action, list):
-    in_file = open(filename, action)
+def load_file(filename, projects):
+    in_file = open(filename, 'r')
     in_file.readline()
     for line in in_file:
         # print(repr(line))  # check line formatting
         parts = line.strip().split('\t')
         # print(parts)  # check formatting
-        date_string = parts[1]  # e.g., "30/9/2022"
-        date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-        project = Project(parts[0], date, int(parts[2]), float(parts[3]), int(parts[4]))
-        list.append(project)
+        project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4]))
+        projects.append(project)
     in_file.close()
-    return list
+    return projects
+
+def save_file(filename, projects):
+    out_file = open(filename, 'w')
+    for project in projects:
+        print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.cost_estimate}\t{project.cpmpletion_percentage}")
+
+
+
 
 
 main()
