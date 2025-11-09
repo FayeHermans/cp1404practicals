d66fa28e250fa0285d07d0d16de393a56671d833
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-09 14:57:09 +1000

    Add all menu choices

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -8,24 +8,59 @@ Actual time:
 from project import Project
 import datetime
 
+MENU = ("- (L)oad projects\n"
+"- (S)ave projects\n"
+"- (D)isplay projects\n"
+"- (F)ilter projects by date\n"
+"- (A)dd new project\n"
+"- (U)pdate project\n"
+"- (Q)uit\n"
+">>> ")
+
 
 def main():
     projects = []
-    process_file('projects.txt', 'r', projects)
+    filename = 'projects.txt'
+    projects = process_file(filename, 'r', projects)
+    print("Welcome to Pythonic Project Management")
+    print(f"Loaded {len(projects)} projects from {filename}")
+    choice = input(MENU).upper()
+    while choice != "Q":
+        if choice == "L":
+            # load project
+            pass
+        elif choice == "S":
+            # save project
+            pass
+        elif choice == "D":
+            # display projects
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid choice.")
+        choice = input(MENU).upper()
+
+
 
 
 def process_file(filename, action, list):
     in_file = open(filename, action)
     in_file.readline()
     for line in in_file:
-        print(repr(line))  # check line formatting
+        # print(repr(line))  # check line formatting
         parts = line.strip().split('\t')
-        print(parts)  # check formatting
+        # print(parts)  # check formatting
         date_string = parts[1]  # e.g., "30/9/2022"
         date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
         project = Project(parts[0], date, int(parts[2]), float(parts[3]), int(parts[4]))
         list.append(project)
     in_file.close()
+    return list
 
 
 main()
