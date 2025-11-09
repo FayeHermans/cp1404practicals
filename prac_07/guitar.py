80551882dc5b96fbcdf20fc8e2049d5caa4e38d4
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-09 13:32:37 +1000

    Copy file over from prac 06

---
+++ prac_07/guitar.py
@@ -0,0 +1,16 @@
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0.00):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        age = 2025 - self.year
+        return age
+
+    def is_vintage(self):
+        return self.get_age() >= 50
