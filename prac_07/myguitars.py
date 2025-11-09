de7e1f8189fb8345f8abe27f40d7150a5bbe2946
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-09 13:43:22 +1000

    Write program to read file and store objectss in a list using a class

---
+++ prac_07/myguitars.py
@@ -0,0 +1,25 @@
+"""
+Prac 07 - myguitars
+Read guitars from file and store them in a list.
+"""
+from guitar import Guitar
+
+
+def main():
+    """Read file and save in list."""
+    myguitars = []
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        # print(repr(line)) # check line formatting
+        parts = line.strip().split(',')
+        # print(parts) # check parts formatting
+        guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        myguitars.append(guitar)
+    in_file.close()
+
+    for myguitar in myguitars:
+        print(myguitar)
+
+
+
+main()
