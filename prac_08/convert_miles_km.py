64be12e1422ab19662b68f47b358d23ffc65588b
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-15 14:38:31 +1000

    Write convert miles km program from scratch

---
+++ prac_08/convert_miles_km.py
@@ -0,0 +1,22 @@
+"""
+CP1404 Prac 08
+GUI program to convert miles to km.
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KM = 1.60934
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+
+
+
+MilesConverterApp().run()
