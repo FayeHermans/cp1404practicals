3ea0772a2ca95c0f9d9a1279e1a8eada4c8da509
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-15 15:35:43 +1000

    Add class app

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,4 +2,24 @@
 CP1404 Prac 8
 App to craate labels for names
 """
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
 
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Faye", "Brody", "Nova"]
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
