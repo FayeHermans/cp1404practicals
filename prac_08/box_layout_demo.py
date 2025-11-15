efc10ee629a400245bf88b8b6a06a8ddb9815281
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-15 13:59:37 +1000

    Add clear function

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -12,4 +12,8 @@ class BoxLayoutDemo(App):
         print("Greet")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
+    def press_clear(self):
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
 BoxLayoutDemo().run()
