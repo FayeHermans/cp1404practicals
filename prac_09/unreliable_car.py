a03cd30170b534062ee9c6dd600b03e2f6023b46
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-23 09:38:01 +1000

    Create UnreliableCar class

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,17 @@
+"""CP1404 Prac 09"""
+import random
+
+from car import Car
+
+class UnreliableCar(Car):
+
+    def __init__(self, name, fuel, reliability: float):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        if random.randint(0, 100) < self.reliability:
+            return self.drive(distance)
+        else:
+            return 0
+
