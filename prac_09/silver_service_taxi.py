55fe36e30748abb35c09daaa95dd95babace74c1
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-23 09:58:03 +1000

    Override str to include flagfall at the end

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -10,3 +10,6 @@ class SilverServiceTaxi(Taxi):
 
     def get_fare(self):
         return self.price_per_km * self.current_fare_distance + self.flagfall
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f} "
