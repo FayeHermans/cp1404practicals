0d866ddcb979ea592991e370833bb3c1d0de6e69
Faye Hermans <pheadra.hermans@my.jcu.edu.au>
2025-11-23 11:26:05 +1000

    Add quit print statement

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,37 +1,42 @@
 """CP1404 Prac 09"""
-from prac_01.menus import choice
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
 MENU = ("q)uit, c)hoose taxi, d)rive\n"
-        ">>>")
+        ">>> ")
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
     bill = 0
     print("Let's drive!")
-    choice - input(MENU).lower()
+    choice = input(MENU).lower()
     while choice != 'q':
         if choice == 'c':
             print("Taxis available:")
             for i in range(0, len(taxis)):
                 print(f"{i} - {taxis[i]}")
-            taxi_choice = input("Choose taxi: ")
-            current_taxi = taxis[taxi_choice]
-            if taxi_choice > len(taxis):
+            taxi_choice = int(input("Choose taxi: "))
+            if taxi_choice >= len(taxis):
                 print("Invalid taxi choice")
+            else:
+                current_taxi = taxis[taxi_choice]
+
         if choice == 'd':
-            distance = input("Drive how far? ")
+            distance = int(input("Drive how far? "))
             Taxi.start_fare(current_taxi)
             Taxi.drive(current_taxi, distance)
             if current_taxi == taxis[0]:
                 trip_fare = Taxi.get_fare(current_taxi)
             else:
                 trip_fare = SilverServiceTaxi.get_fare(current_taxi)
-            print(f"Your {current_taxi.name} trip cost you ${trip_fare}")
+            print(f"Your {current_taxi.name} trip cost you ${trip_fare:.2f}")
             bill += trip_fare
-        print(bill)
+        print(f"Bill to date: ${bill:.2f}")
+        choice = input(MENU).lower()
+    print(f"Total trip cost: ${bill:.2f}")
+    print("Taxis are now:")
+
 
 
 
